0d4921ef1a14d4201068c29a83ab5220882ec65f
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-08-08 08:28:38 -0700

    can walk slices, primtives

--- reflectwalk.go
+++ reflectwalk.go
@@ -4,6 +4,20 @@ import (
 	"reflect"
 )
 
+// PrimitiveWalker implementations are able to handle primitive values
+// within complex structures. Primitive values are numbers, strings,
+// and booleans. These primitive values are often members of more complex
+// structures (slices, maps, etc.) that are walkable by other interfaces.
+type PrimitiveWalker interface {
+	Primitive(reflect.Value) error
+}
+
+// SliceWalker implementations are able to handle slice elements found
+// within complex structures.
+type SliceWalker interface {
+	SliceElem(int, reflect.Value) error
+}
+
 // StructWalker is an interface that has methods that are called for
 // structs when a Walk is done.
 type StructWalker interface {
@@ -16,12 +30,52 @@ func Walk(data, walker interface{}) error {
 }
 
 func walk(v reflect.Value, w interface{}) error {
-	switch v.Kind() {
+	k := v.Kind()
+	if k >= reflect.Int && k <= reflect.Complex128 {
+		k = reflect.Int
+	}
+
+	switch k {
+	// Primitives
+	case reflect.Bool:
+		fallthrough
+	case reflect.Int:
+		fallthrough
+	case reflect.String:
+		return walkPrimitive(v, w)
+	case reflect.Slice:
+		return walkSlice(v, w)
 	case reflect.Struct:
 		return walkStruct(v, w)
 	default:
 		return nil
 	}
+}
+
+func walkPrimitive(v reflect.Value, w interface{}) error {
+	if pw, ok := w.(PrimitiveWalker); ok {
+		return pw.Primitive(v)
+	}
+
+	return nil
+}
+
+func walkSlice(v reflect.Value, w interface{}) (err error) {
+	for i := 0; i < v.Len(); i++ {
+		elem := v.Index(i)
+
+		if sw, ok := w.(SliceWalker); ok {
+			if err := sw.SliceElem(i, elem); err != nil {
+				return err
+			}
+		}
+
+		if err := walk(elem, w); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func walkStruct(v reflect.Value, w interface{}) (err error) {
--- reflectwalk_test.go
+++ reflectwalk_test.go
@@ -5,30 +5,99 @@ import (
 	"testing"
 )
 
-type TestStructWalker struct {
-	StructFieldFunc func(reflect.StructField, reflect.Value) error
+type TestPrimitiveWalker struct {
+	Value reflect.Value
 }
 
-func (t TestStructWalker) StructField(sf reflect.StructField, v reflect.Value) error {
-	return t.StructFieldFunc(sf, v)
+func (t *TestPrimitiveWalker) Primitive(v reflect.Value) error {
+	t.Value = v
+	return nil
+}
+
+type TestSliceWalker struct {
+	Count int
+}
+
+func (t *TestSliceWalker) SliceElem(int, reflect.Value) error {
+	t.Count++
+	return nil
+}
+
+type TestStructWalker struct {
+	Fields []string
+}
+
+func (t *TestStructWalker) StructField(sf reflect.StructField, v reflect.Value) error {
+	if t.Fields == nil {
+		t.Fields = make([]string, 0, 1)
+	}
+
+	t.Fields = append(t.Fields, sf.Name)
+	return nil
 }
 
 func TestTestStructs(t *testing.T) {
 	var raw interface{}
-	raw = TestStructWalker{}
+	raw = new(TestPrimitiveWalker)
+	if _, ok := raw.(PrimitiveWalker); !ok {
+		t.Fatal("PrimitiveWalker is bad")
+	}
+
+	raw = new(TestSliceWalker)
+	if _, ok := raw.(SliceWalker); !ok {
+		t.Fatal("SliceWalker is bad")
+	}
+
+	raw = new(TestStructWalker)
 	if _, ok := raw.(StructWalker); !ok {
 		t.Fatal("StructWalker is bad")
 	}
 }
 
-func TestWalk_Struct(t *testing.T) {
-	fields := make([]string, 0)
-	w := &TestStructWalker{
-		StructFieldFunc: func(sf reflect.StructField, v reflect.Value) error {
-			fields = append(fields, sf.Name)
-			return nil
-		},
+func TestWalk_Basic(t *testing.T) {
+	w := new(TestPrimitiveWalker)
+
+	type S struct {
+		Foo string
 	}
+
+	data := &S{
+		Foo: "foo",
+	}
+
+	err := Walk(data, w)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	if w.Value.Kind() != reflect.String {
+		t.Fatalf("bad: %#v", w.Value)
+	}
+}
+
+func TestWalk_Slice(t *testing.T) {
+	w := new(TestSliceWalker)
+
+	type S struct {
+		Foo []string
+	}
+
+	data := &S{
+		Foo: []string{"a", "b", "c"},
+	}
+
+	err := Walk(data, w)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	if w.Count != 3 {
+		t.Fatalf("Bad count: %d", w.Count)
+	}
+}
+
+func TestWalk_Struct(t *testing.T) {
+	w := new(TestStructWalker)
 
 	type S struct {
 		Foo string
@@ -46,7 +115,7 @@ func TestWalk_Struct(t *testing.T) {
 	}
 
 	expected := []string{"Foo", "Bar"}
-	if !reflect.DeepEqual(fields, expected) {
-		t.Fatalf("bad: %#v", fields)
+	if !reflect.DeepEqual(w.Fields, expected) {
+		t.Fatalf("bad: %#v", w.Fields)
 	}
 }
